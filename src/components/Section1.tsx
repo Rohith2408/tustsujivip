3c36b1949d4b6dbbf196d5635d8dbc46acadea4b
Rohith2408 <kumarrohith24081999@gmail.com>
2024-07-30 04:37:01 +0530

    init

---
+++ src/components/Section1.tsx
@@ -0,0 +1,29 @@
+import styles from "../components/Section1.module.css"
+import React from 'react'
+import bg from '../Images/1/main.png'
+
+const Section1=()=>{
+
+    return(
+        <div className={styles.mainWrapper}>
+            <div className={styles.subWrapper}>
+                <div className={styles.body}>
+                    <p className={styles.text1}>Hie Everyone,</p>
+                    <p className={styles.text1}>My Name is</p>
+                    <p className={styles.text1} style={{color:"#FF6868"}}>tustsuji</p>
+                    <p className={styles.text2}>The most adorable animals on Solana</p>
+                </div>
+                <div className={styles.imageWrapper}>
+                    <img
+                        className={styles.image}
+                        loading="lazy"
+                        alt=""
+                        src={bg}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section1
