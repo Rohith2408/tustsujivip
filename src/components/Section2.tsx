a3e7c4168bbbfa5ff9161be0fe6efa70957c35dc
Rohith2408 <kumarrohith24081999@gmail.com>
2024-07-30 04:41:18 +0530

    update

---
+++ src/components/Section2.tsx
@@ -0,0 +1,20 @@
+import styles from "../components/Section2.module.css"
+import React from 'react'
+
+const Section2=()=>{
+
+    return(
+        <div className={styles.mainWrapper}>
+            <div className={styles.subWrapper}>
+                <div className={styles.body}>
+                   
+                </div>
+                <div className={styles.imageWrapper}>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section2
